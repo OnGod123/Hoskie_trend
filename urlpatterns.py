cf77e69109921a2f6136904eb0cf4abfd5d50f49
OnGod123 <ifejikav4@gmail.com>
2024-10-11 21:27:42 +0100

    new_trend

--- urlpatterns.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import submit_message, get_trends
-
-urlpatterns = [
-    path('submit/', submit_message, name='submit_message'),
-    path('trends/', get_trends, name='get_trends'),
-]
